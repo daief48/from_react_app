9502f7bd18081689102dd6639fa5171e5c7c56f4
daief48 <daief20103048@gmail.com>
2023-05-04 01:17:35 +0600

    add tow more input done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,96 @@
 import React, { useState } from 'react'
 export default function App() {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState('');
-  const [fullname, setFullname] = useState();
-  const [lastNamenew, setLastNameNew] = useState('');
-  const inputEvent = (event) => {
-    console.log(event.target.value);
-    setName(event.target.value);
-  }
-  const inputEventTwo = (event) => {
-    console.log(event.target.value);
-    setLastName(event.target.value);
-  }
+  const [fullname, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email:"",
+    phone:"",
+  });
+
   const onSubmits = (event) => {
     event.preventDefault();
-    setFullname(name);
-    setLastNameNew(lastName);
-  } 
 
-  // const onSubmit = () => {
-  //   setFullname(name)
-  // }
+  }
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    // const value = event.target.value;
+    // const name = event.target.name;
+
+    const {value , name } = event.target;
+
+    setFullName((preValue) => {
+
+      if (name === "fName") {
+        return ({
+          fname: value,
+          lname: preValue.lname,
+          email:preValue.email,
+          phone:preValue.phone,
+        })
+      } else if (name === 'lName') {
+        return ({
+          fname: preValue.fname,
+          lname: value,
+          email:preValue.email,
+          phone:preValue.phone,
+        })
+      }else if (name === 'email') {
+        return ({
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email:value,
+          phone:preValue.phone,
+        })
+      }else if (name === 'phone') {
+        return ({
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email:preValue.email,
+          phone:value,
+        })
+      }
+    })
+  }
+
+
+
   return (
     <>
       <div className='main_div'>
         <form onSubmit={onSubmits}>
           <div>
-            <h1>Hello {fullname} {lastNamenew}</h1>
+            <h1>Hello {fullname.fname} {fullname.lname}</h1>
+            <p>{fullname.email}</p>
+            <p>{fullname.phone}</p>
             <input type="text"
               placeholder='Enter your First Name'
+              name='fName'
               onChange={inputEvent}
-              value={name}
+              value={fullname.fname}
             />
             <br />
             <input type="text"
               placeholder='Enter your Last Name'
-              onChange={inputEventTwo}
-              value={lastName}
+              name='lName'
+              onChange={inputEvent}
+              value={fullname.lname}
+            />
+            <br />
+            <input type="email"
+              placeholder='Enter your Last Name'
+              name='email'
+              onChange={inputEvent}
+              value={fullname.email}
+            />
+            <br />
+            <input type="number"
+              placeholder='Enter your Last Name'
+              name='phone'
+              onChange={inputEvent}
+              value={fullname.phone}
             />
             <button>Click Me 🤜</button>
 
