c10f74328ef4eb958fbeeb4833f9fc313b80441a
daief48 <daief20103048@gmail.com>
2023-05-04 01:29:34 +0600

    app complete

--- src/App.js
+++ src/App.js
@@ -23,35 +23,39 @@ export default function App() {
 
     setFullName((preValue) => {
 
-      if (name === "fName") {
-        return ({
-          fname: value,
-          lname: preValue.lname,
-          email:preValue.email,
-          phone:preValue.phone,
-        })
-      } else if (name === 'lName') {
-        return ({
-          fname: preValue.fname,
-          lname: value,
-          email:preValue.email,
-          phone:preValue.phone,
-        })
-      }else if (name === 'email') {
-        return ({
-          fname: preValue.fname,
-          lname: preValue.lname,
-          email:value,
-          phone:preValue.phone,
-        })
-      }else if (name === 'phone') {
-        return ({
-          fname: preValue.fname,
-          lname: preValue.lname,
-          email:preValue.email,
-          phone:value,
-        })
+      return{
+        ...preValue,
+        [name]:value
       }
+      // if (name === "fName") {
+      //   return ({
+      //     fname: value,
+      //     lname: preValue.lname,
+      //     email:preValue.email,
+      //     phone:preValue.phone,
+      //   })
+      // } else if (name === 'lName') {
+      //   return ({
+      //     fname: preValue.fname,
+      //     lname: value,
+      //     email:preValue.email,
+      //     phone:preValue.phone,
+      //   })
+      // }else if (name === 'email') {
+      //   return ({
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     email:value,
+      //     phone:preValue.phone,
+      //   })
+      // }else if (name === 'phone') {
+      //   return ({
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     email:preValue.email,
+      //     phone:value,
+      //   })
+      // }
     })
   }
 
@@ -67,14 +71,14 @@ export default function App() {
             <p>{fullname.phone}</p>
             <input type="text"
               placeholder='Enter your First Name'
-              name='fName'
+              name='fname'
               onChange={inputEvent}
               value={fullname.fname}
             />
             <br />
             <input type="text"
               placeholder='Enter your Last Name'
-              name='lName'
+              name='lname'
               onChange={inputEvent}
               value={fullname.lname}
             />
