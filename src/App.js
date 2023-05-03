e277f69347a2a6668c745c3e133d13b0c990a32d
daief48 <daief20103048@gmail.com>
2023-05-04 00:55:29 +0600

    added input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 export default function App() {
   const [name, setName] = useState("");
+  const [lastName, setLastName] = useState('');
   const [fullname, setFullname] = useState();
+  const [lastNamenew, setLastNameNew] = useState('');
   const inputEvent = (event) => {
     console.log(event.target.value);
     setName(event.target.value);
   }
-
+  const inputEventTwo = (event) => {
+    console.log(event.target.value);
+    setLastName(event.target.value);
+  }
   const onSubmits = (event) => {
     event.preventDefault();
-    setFullname(name)
+    setFullname(name);
+    setLastNameNew(lastName);
   } 
 
   // const onSubmit = () => {
@@ -20,11 +26,17 @@ export default function App() {
       <div className='main_div'>
         <form onSubmit={onSubmits}>
           <div>
-            <h1>Hello {fullname}</h1>
+            <h1>Hello {fullname} {lastNamenew}</h1>
             <input type="text"
-              placeholder='Enter your Name'
+              placeholder='Enter your First Name'
               onChange={inputEvent}
               value={name}
+            />
+            <br />
+            <input type="text"
+              placeholder='Enter your Last Name'
+              onChange={inputEventTwo}
+              value={lastName}
             />
             <button>Click Me 🤜</button>
 
